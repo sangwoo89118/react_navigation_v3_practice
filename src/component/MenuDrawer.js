0968255458b4c1fbbb5bdb0b606120e6f5f5ba8d
sangwoo89118 <sangwoo89118@gmail.com>
2019-03-02 03:43:44 -0800

    finished menu drawer

--- src/component/MenuDrawer.js
+++ src/component/MenuDrawer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, Platform, Dimensions, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, Platform, Dimensions, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native'
 
 const WIDTH = Dimensions.get('window').width;
 const HEIGHT = Dimensions.get('window').height;
@@ -15,13 +15,28 @@ export default class MenuDrawer extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.topLinks}>
-                    <Text style={{color: 'white', paddingTop: 40}}>MenuDrawer</Text>
-                </View>
-                <View style={styles.bottomLinks}>
-                    {this.navLink('Home', 'Home')}
-                    {this.navLink('Links', 'Links')}
-                    {this.navLink('Settings', 'Settings')}
+                <ScrollView style={styles.scroller}>
+                    <View style={styles.topLinks}>
+                        <View style={styles.profile}>
+                            <View style={styles.imgView}>
+                                <Image style={styles.img} source={require('../../assets/profile_2.png')} />
+                            </View>
+                            <View style={styles.profileText}>
+                                <Text style={styles.name}>
+                                    Sangwoo Kim
+                                </Text>                        
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.bottomLinks}>
+                        {this.navLink('Home', 'Home')}
+                        {this.navLink('Links', 'Links')}
+                        {this.navLink('Settings', 'Settings')}
+                    </View>
+                </ScrollView>
+                <View style={styles.footer}>
+                    <Text style={styles.description}>Menu Tutorial</Text>
+                    <Text style={styles.version}>v1.0</Text>
                 </View>
             </View>
         )
@@ -32,6 +47,38 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'lightgray',
+    },
+    scroller: {
+        flex: 1,
+    },
+    profile: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 25,
+        borderBottomWidth: 1,
+        borderBottomColor: '#777777',
+    },
+    profileText: {
+        flex: 3,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    name: {
+        fontSize: 20,
+        paddingBottom: 5,
+        color: 'white',
+        textAlign: 'left'
+    },
+    imgView: {
+        flex: 1,
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+    img: {
+        height: 70,
+        width: 70,
+        borderRadius: 35,
     },
     topLinks: {
         height: 160,
@@ -51,4 +98,23 @@ const styles = StyleSheet.create({
         margin: 5,
         textAlign: 'left',
     },
+    footer: {
+        height: 50,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderTopWidth: 1,
+        borderTopColor: 'lightgray',
+    },
+    version: {
+        flex: 1,
+        textAlign: 'right',
+        marginRight: 20,
+        color: 'gray',
+    },
+    description: {
+        flex: 1,
+        marginLeft: 20,
+        fontSize: 16
+    }
 })
